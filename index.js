fff54997303916832c701b246b9abd97ea09fcc5
JannaturNur08 <nur@playense.com>
2023-12-22 17:37:47 +0600

    filtering tasks with email

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
@@ -23,22 +23,58 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
+		const taskCollection = client.db("TaskManagement").collection("task");
 
-      const taskCollection = client.db("TaskManagement").collection("task");
+		// post task
+		app.post("/tasks", async (req, res) => {
+			const newTask = req.body;
+			const task = await taskCollection.insertOne(newTask);
+			res.send(task);
+		});
 
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
+		
+		//get task by email
+		app.get("/myTasks", async (req, res) => {
+			const email = req.query.email;
+			console.log(email);
+			const query = { email: email };
+			const result = await taskCollection.find(query).toArray();
+			res.send(result);
+		});
+
+		//task status update
+		app.put("/tasks/:id", async (req, res) => {
+			const { status } = req.body;
+			const taskId = req.params.id;
+
+			let update = {
+				$set: {
+					status: status,
+				},
+			};
+
+			const result = await taskCollection.updateOne(
+				{ _id: new ObjectId(taskId) },
+				update
+			);
+			res.send(result);
+		});
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		//await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
 	res.send("Task Management is running!");
